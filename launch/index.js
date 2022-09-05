9d76e4d5e260e9013b1128ef928d4d906798c217
unknown <jordiromijn@gmail.com>
2022-09-05 15:40:55 +0200

    fix

--- launch/index.js
+++ launch/index.js
@@ -2,8 +2,7 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on('ready', () => {
-  console.log(`Logged in...`)
-  .setTitle('bier is goed voor je')
+  console.log(`Logged in...`);
 });
 
 client.on('message', msg => {
