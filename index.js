d63a662e015682ffe369283f20457fca78b6c65a
unknown <jordiromijn@gmail.com>
2022-09-05 14:39:08 +0200

    setup

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in...`);
+  console.log(`Logged in...`)
+  .setTitle('bier is goed voor je')
 });
 
 client.on('message', msg => {
