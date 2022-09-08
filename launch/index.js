06c462e80fc70b4d4f0dd5954e13cbca481db75f
unknown <jordiromijn@gmail.com>
2022-09-08 14:34:13 +0200

    feat: help

--- launch/index.js
+++ launch/index.js
@@ -1,11 +1,13 @@
 const { Client, Intents, MessageEmbed } = require("discord.js");
-const client = new Client({ intents: Object.values(Intents.FLAGS).reduce((a, b) => a + b) });
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+  partials: ["CHANNEL", "MESSAGE", "REACTION"],
+});
 
 // const help = require("./help.js");
 const { prefix } = require("./config.json");
 
 client.on("ready", () => {
-  console.log(`Logged in...`);
   client.user.setActivity("dikke", {
     type: "STREAMING",
     url: "https://www.twitch.tv/amouranth",
