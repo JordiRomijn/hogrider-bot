889a6223551264534fcc97d321f15f03535322cd
unknown <jordiromijn@gmail.com>
2022-09-08 14:48:47 +0200

    feat: help

--- launch/index.js
+++ launch/index.js
@@ -1,7 +1,6 @@
 const { Client, Intents } = require("discord.js");
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
-  partials: ["CHANNEL", "MESSAGE", "REACTION"],
 });
 
 // const help = require("./help.js");
