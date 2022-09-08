0e77cedaa371aee69c03ec2a53b8f0d3c17fee7e
unknown <jordiromijn@gmail.com>
2022-09-08 14:18:19 +0200

    feat: help

---
+++ launch/help.js
@@ -0,0 +1,13 @@
+const help = new MessageEmbed()
+.setDescription(`**Jordi's bot ;**
+\n**Create Channels ;**
+${prefix}cc [amount] (text) i.e \`${prefix}cc 5 test\`\n
+**Create Channels & Ping ;**
+More Coming Soon!
+Made By: \`Jordi\`\n Discord: \`Jordi#5669\`\n
+`)
+.setFooter(`© Rice Beamer Beta`, 'https://st2.depositphotos.com/1864495/7208/i/600/depositphotos_72082789-stock-photo-trollface-dark-spectacled-internet-troll.jpg', 'https://sullylovesmelons.xyz')
+.setColor(0x00FFFF)
+.setAuthor('Riceblades11', 'https://st2.depositphotos.com/1864495/7208/i/600/depositphotos_72082789-stock-photo-trollface-dark-spectacled-internet-troll.jpg', 'https://sullylovesmelons.xyz')
+.setThumbnail("https://media.istockphoto.com/illustrations/trollface-internet-troll-3d-illustration-illustration-id499870283?k=20&m=499870283&s=612x612&w=0&h=_oy1OzNBqcJhYIQmJeGxO322DPlzhupXlSRgL6u9Pwg=")
+.setTimestamp(Date.now());
--- launch/index.js
+++ launch/index.js
@@ -1,19 +1,28 @@
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] ,
-  partials: ['CHANNEL', 'MESSAGE', 'REACTION'] });
+const { Client, Intents } = require("discord.js");
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+  partials: ["CHANNEL", "MESSAGE", "REACTION"],
+});
 
-client.on('ready', () => {
+const help = require("help.js");
+const { prefix } = require("config.json")
+
+client.on("ready", () => {
   console.log(`Logged in...`);
   client.user.setActivity("dikke", {
     type: "STREAMING",
-    url: "https://www.twitch.tv/amouranth"
+    url: "https://www.twitch.tv/amouranth",
   });
 });
 
-client.on('messageCreate', (msg) => {
-   if (msg.content === 'Hello') {
-     msg.reply(`Hello ${msg.author.username}`);
-   }
-  });
+client.on("messageCreate", (msg) => {
+  if (msg.content === "Hello") {
+    msg.reply(`Hello ${msg.author.username}`);
+  }
+
+  if (message.content.startsWith(prefix + "help")) {
+    message.channel.send({ embeds: [help] });
+  }
+});
 
 client.login(process.env.token);
