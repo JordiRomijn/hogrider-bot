d94d390f539f9011e05f1c8be5cacb6366b3759d
unknown <jordiromijn@gmail.com>
2022-09-08 13:47:01 +0200

    test

--- launch/index.js
+++ launch/index.js
@@ -1,12 +1,14 @@
-const { Client, Intents, Message } = require('discord.js');
+const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on('ready', () => {
   console.log(`Logged in...`);
 });
 
-if(message.content == 'bier') {
-  message.channel.sendMessage('lekker');
-}
+client.on('message', msg => {
+  if (msg.content == 'ping') {
+    msg.reply('Pong!');
+  }
+});
 
 client.login(process.env.token);
