6aab3aa12670459a6372d604ff09fb9d8eb95129
unknown <jordiromijn@gmail.com>
2022-09-08 14:00:00 +0200

    test

--- launch/index.js
+++ launch/index.js
@@ -1,5 +1,6 @@
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] ,
+  partials: ['CHANNEL', 'MESSAGE', 'REACTION'] });
 
 client.on('ready', () => {
   console.log(`Logged in...`);
@@ -7,7 +8,7 @@ client.on('ready', () => {
 
 client.on("messageCreate", (message) => {
   if (message.content.startsWith("bier")) {
-    message.channel.send('bjiwefb')
+    message.channel.send('bjiwefb');
 }
 });
 
